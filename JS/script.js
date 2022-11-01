72a255343db8c2f855063283aa8696b19761ab0d
szaccarias <s.zaccarias@outlook.com>
2022-10-31 21:58:56 -0300

    fix

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-/*SIMULADOR DE COTIZACIONES DE SEGUROS DE AUTO*/
+/* Simulador - Cotizador de Automotores*/
 class Cotizaciones { 
     constructor (seleccioneMarca, precioPorYear, precioPorTipo) {
         this.brand = seleccioneMarca;
@@ -7,26 +7,14 @@ class Cotizaciones {
     }
 }
 
-/*FUNCION PARA MULTIPLICAR */
+/* Función Multiplicar */
 function multiplicacion(a, b){
     return a * b
 }
 
 const seguroBase = 2000
 
- 
-/*AUMENOS SEGUN MARCAS DE AUTO*/
-/*OBJETOS DE LAS MARCAS DE AUTO*/
-/*let marcasDeAuto = {
-    toyota: 1.5,
-    volkswagen: 1.2,
-    ford: 1.05,
-    chevrolet: 1.15,
-    renault: 1.08,
-    audi: 2  
-}*/
- 
-/*MODIFIQUE E HICE UN OBJETO CONSTRUCTOR DE CADA MARCA */
+/*Objeto contructor de cada Marca*/
 
 const idGenerator = () => {
 return Math.round(Math.random() * 100)}
@@ -46,8 +34,7 @@ let marcaChevrolet = new Cars(idGenerator(), "Chevrolet", 1.15)
 let marcaRenault = new Cars(idGenerator(), "Renault", 1.08)
 let marcaAudi = new Cars(idGenerator(), "Audi", 2)
 
-
-/*HACEMOS UN ARRAY CON SOLO EL NOMBRE DE LAS MARCAS */
+/*Arrays: Solamente con el nombre de las Marcas*/
 
 const arrayMarcas = [marcaToyota, marcaVolkswagen, marcaFord, marcaChevrolet, marcaRenault, marcaAudi]
 const listaMarcas = []
@@ -57,35 +44,22 @@ for (nombreMarca of arrayMarcas) {
 }
 console.log(listaMarcas.join(", "))
 
-/*PRUEBA DE INCLUDES() */
+/* Probando Includes() */
+
 const filtradoMarcas = listaMarcas.filter((searchBrands) => searchBrands.includes("T"))
 console.log(filtradoMarcas)
 
-/*const filtradoNombre = arrayMarcas.filter((searchBrands) => searchBrands.costoBase > 1.2)
-console.log(filtradoNombre)*/
-
-/*MAP CON AUMENTO DE PRECIOS*/
+/*Map aumento de precios*/
 const brandPrice = arrayMarcas.map((el) => el.costoBase*1.2) 
 console.log(brandPrice)
 
-/*MAP PARA VER LOS ID GENERADOS (PRUEBA) */
+/* Probando Map ID generados*/
 let idMarcas = arrayMarcas.map((prod) => prod.id)
-/*console.log(idMarcas)*/
 
-/*let seleccionTipoDeSeguro = {
-    respCivil: 2000,
-    tercerosCompletos: 2000,
-    todoRiesgo: 2500
-}
-const respCivil = 2000
-const tercerosCompletos = 2200
-const todoRiesgo = 2500*/
-
- 
 let precioParcial = 0
     
+/* Datos Personales del Usuario*/
 
-/*DATOS PERSONALES*/
 const datosPersonales = []
 
 let inputName = document.getElementById("name")
@@ -110,12 +84,6 @@ function mostrarPorConsola(nombre){
     console.log("Bienvenido" + nombre)
 }
 
-
-
-/*const clientNames = datosPersonales.filter((cliente) => cliente.nombre.includes("A"))
-console.log(clientNames)*/
-
-
 function precioSegunMarca(base, marca){
     precioParcial = multiplicacion(base, marca)
 }
@@ -130,11 +98,8 @@ let brand = document.getElementById("brand");
         <option id="selectBrand" value="Renault">${marcaRenault.brands}</option>
         <option id="selectBrand" value="Audi">${marcaAudi.brands}</option>          
         `
-    
-    brand.addEventListener('change', seleccioneMarca)
-    
-    //()=>{brand = opcionesASeleccionarMarca.value}
 
+    brand.addEventListener('change', seleccioneMarca)
     
 function seleccioneMarca () {
     if(brand.value == marcaToyota.brands){
@@ -143,27 +108,22 @@ function seleccioneMarca () {
 
     else if(brand.value == marcaVolkswagen.brands){
         precioSegunMarca(seguroBase, marcaVolkswagen.costoBase)
-        /*precioParcial = multiplicacion(seguroBase, marcasDeAuto.volkswagen)*/
         }
 
         else if(brand.value == marcaFord.brands){
         precioSegunMarca(seguroBase, marcaFord.costoBase)
-        /*precioParcial = multiplicacion(seguroBase, marcasDeAuto.ford)*/
         }
 
     else if(brand.value == marcaChevrolet.brands){
         precioSegunMarca(seguroBase, marcaChevrolet.costoBase)
-        /*precioParcial = multiplicacion(seguroBase, marcasDeAuto.chevrolet)*/
         }
 
     else if(brand.value == marcaRenault.brands){
         precioSegunMarca(seguroBase, marcaRenault.costoBase)
-        /*precioParcial = multiplicacion(seguroBase, marcasDeAuto.renault)*/
         }
 
     else if(brand.value == marcaAudi.brands){
         precioSegunMarca(seguroBase, marcaAudi.costoBase)
-        /*precioParcial = multiplicacion(seguroBase, marcasDeAuto.audi)*/
         }
 
     else {
@@ -171,9 +131,9 @@ function seleccioneMarca () {
         
     }         console.log(precioParcial)
 }
-        
-                
-/*CALCULO DE PRECIO DE ACUERDO AL AÑO DEL AUTO */
+                   
+/* Calculo de costo por año del vehículo*/
+
 let precioParcial2
 
 let year = document.getElementById("anio")
@@ -184,7 +144,7 @@ const añoActual = 2022
 
 if(year.value != (" ")){
 let diferencia = añoActual - year.value
-let calculoDiferenciaPorAño = ((diferencia * precioParcial) * 3 )/ 100 /*CUANTO ES LO QUE SE AUMENTA (CALCULANDO EL 3% DE LA DIFERENCIA EN RELACION AL PRECIO PARCIAL)*/
+let calculoDiferenciaPorAño = ((diferencia * precioParcial) * 3 )/ 100 /* Lo que aumenta (Calculando el 3% de la diferencia en relación al precio parcial)*/
 
 precioParcial2 = precioParcial + calculoDiferenciaPorAño
 }
@@ -195,19 +155,12 @@ else{
 console.log(precioParcial2)
 }
 
-/*alert ("Su precio parcial es:" + " " + "$" + precioParcial)*/
-
-/*AUMENTOS POR TIPO DE SEGURO
-Responsabilidad Civil: Aumenta un 30%
-Terceros completos aumenta un 50%
-Todo Riesgo: Aumenta un 62%
-*/
-
 let precioTotal = 0
 
-/*OBJETO DE LOS TIPOS DE SEGUROS*/
+/* Objeto de los tipos de Coberturas*/
 
-/*FUNCION CONSTRUCTORA DE LOS TIPOS DE SEGURO */
+/* Funcion constructora de los tipos de Coberturas*/
+
 class Type {
     constructor (tipo, costoPorTipo, _nombre) {
         this.tipo = tipo;
@@ -228,12 +181,6 @@ for (const productoSeguro of productosSeguro){
     listaNombreProducto.push(productoSeguro._nombre)
 }
 console.log(listaNombreProducto)
-
-/*let tipoDeSeguro = {
-    respCivil: 0.3,
-    tercerosCompletos: 0.5,
-    todoRiesgo: 0.6
-}*/
 
 let precioType = 0
 
@@ -256,21 +203,15 @@ function precioPorTipo(){
     console.log(Number.isFinite(precioType))
 }
 
-/*CALCULO DEL COSTO TOTAL DEL SEGURO*/
+/* Calculo final costo total del Seguro*/
 
 let boton = document.getElementById("botonCotizar")
 
-/*boton.addEventListener("onmousemove", estiloBoton)
-function estiloBoton(){
-    boton.style.backgroundColor = "red";
-}*/
-
 boton.addEventListener("click", costoTotal)
-
 
 function costoTotal (){
 precioTotal = Math.round(precioType + precioParcial2 +precioParcial)
-/*alert("El costo total es:" + " " + "$" + precioTotal)*/
+
 document.getElementById("costoDelSeguro")
 costoDelSeguro.innerText = "El costo del seguro es:" + " " + "$" + precioTotal
 
